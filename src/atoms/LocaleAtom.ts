46c1d5533cc07ac8d747a94285566d2538e63de1
David Wheatley <hi@davwheat.dev>
2021-06-22 19:31:01 +0100

    Validate the selected locale

--- src/atoms/LocaleAtom.ts
+++ src/atoms/LocaleAtom.ts
@@ -1,6 +1,15 @@
+import { AvailableLocales } from '@locales';
 import { persistentAtom } from 'recoil-persistence/react';
 
-export const LocaleAtom = persistentAtom({
-  key: 'locale',
-  default: 'en',
-});
+export const LocaleAtom = persistentAtom(
+  {
+    key: 'locale',
+    default: 'en',
+  },
+  {
+    // If the selected language is unavailable, we'll reset to English
+    validator: (data) => {
+      return AvailableLocales.some((locale) => locale.languageCode === data);
+    },
+  }
+);
