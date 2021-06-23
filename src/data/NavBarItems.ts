c009a22ab3f60880e10ec1fba2a541e2bfc9b1e6
David Wheatley <hi@davwheat.dev>
2021-06-23 14:35:39 +0100

    Update nav bar items

--- src/data/NavBarItems.ts
+++ src/data/NavBarItems.ts
@@ -1,14 +1,12 @@
-interface NavBarItem {
+import { AvailableLocales } from '@locales';
+import useLocale from '@hooks/useLocale';
+import setLocale from '../hooks/setLocale';
+
+interface INavBarItemPartial {
   /**
    * Text that represents the nav item.
    */
   title: string | (() => string);
-  /**
-   * URL of the nav item.
-   *
-   * Should start `http(s)`, or be a path relative to the root of the site.
-   */
-  url: string | (() => string);
   /**
    * Whether the nav item URL is external.
    */
@@ -16,8 +14,26 @@ interface NavBarItem {
   /**
    * A function that accepts the current page URL and returns whether the link should be highlighted.
    */
-  shouldHighlight: (url: string) => boolean;
+  shouldHighlight: boolean | ((url: string) => boolean);
 }
+
+type NavBarItem = INavBarItemPartial &
+  (
+    | {
+        /**
+         * URL of the nav item.
+         *
+         * Should start `http(s)`, or be a path relative to the root of the site.
+         */
+        url: string | (() => string);
+      }
+    | {
+        /**
+         * Function to run when this item is selected.
+         */
+        onClick: () => void;
+      }
+  );
 
 interface NavBarGroup {
   /**
@@ -30,7 +46,7 @@ interface NavBarGroup {
    * Can either be an array of items, or a callable
    * method that returns an array of items.
    */
-  items: NavBarItem[] | (() => NavBarItem[]);
+  items: ReadonlyArray<NavBarItem> | (() => ReadonlyArray<NavBarItem>);
   /**
    * URL of the nav group. Optional.
    *
@@ -40,7 +56,7 @@ interface NavBarGroup {
   /**
    * A function that accepts the current page URL and returns whether the group should be highlighted.
    */
-  shouldHighlight?: (url: string) => boolean;
+  shouldHighlight?: boolean | ((url: string) => boolean);
 }
 
 type NavBarData = ReadonlyArray<NavBarItem | NavBarGroup>;
@@ -50,5 +66,74 @@ type NavBarData = ReadonlyArray<NavBarItem | NavBarGroup>;
  *
  * The side nav can contain Groups and Items themselves. Groups
  * can contain multiple items.
+ *
+ * This function must be called from within a React component
+ * to ensure that required data can be fetched from the active
+ * React context.
  */
-export const NavBarItems: NavBarData = [] as const;
+export const NavBarItems = (): NavBarData => {
+  const activeLocale = useLocale();
+
+  return [
+    {
+      title: activeLocale.trans('navBar.items.guide'),
+      externalLink: false,
+      url: '/',
+      shouldHighlight: (slug) => {
+        return slug === '/';
+      },
+    },
+    {
+      title: activeLocale.trans('navBar.items.extend'),
+      externalLink: false,
+      url: '/extend',
+      shouldHighlight: (slug) => {
+        return slug === '/extend' || slug.startsWith('/extend/');
+      },
+    },
+    {
+      title: activeLocale.trans('navBar.items.apiReference'),
+      externalLink: true,
+      url: 'https://api.docs.flarum.org/',
+      shouldHighlight: false,
+    },
+    {
+      title: activeLocale.trans('navBar.groups.flarum.groupTitle'),
+      shouldHighlight: false,
+      items: [
+        {
+          title: activeLocale.trans('navBar.groups.flarum.items.site'),
+          externalLink: true,
+          url: 'https://flarum.org/',
+          shouldHighlight: false,
+        },
+        {
+          title: activeLocale.trans('navBar.groups.flarum.items.discuss'),
+          externalLink: true,
+          url: 'https://discuss.flarum.org/',
+          shouldHighlight: false,
+        },
+        {
+          title: activeLocale.trans('navBar.groups.flarum.items.github'),
+          externalLink: true,
+          url: 'https://github.com/flarum',
+          shouldHighlight: false,
+        },
+      ],
+    },
+    {
+      title: activeLocale.trans('navBar.groups.languages.groupTitle'),
+      shouldHighlight: false,
+      items: AvailableLocales.map(
+        (locale): NavBarItem => ({
+          title: locale.nativeLanguageName,
+          externalLink: false,
+          onClick: () => {
+            setLocale(locale.languageCode);
+          },
+          shouldHighlight: locale.languageCode === activeLocale.metadata.languageCode,
+        })
+      ),
+    },
+  ] as const;
+};
