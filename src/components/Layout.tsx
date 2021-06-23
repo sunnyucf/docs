072b90a4edb728f0ce7618a7d2d12207b0f3436e
David Wheatley <hi@davwheat.dev>
2021-06-23 14:37:18 +0100

    Update page layout template

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { Footer } from './Footer';
 
 import { Header } from './Header';
 
@@ -8,21 +8,23 @@ interface ILayoutProps {
 }
 
 export default function Layout({ children }: ILayoutProps) {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `);
 
   return (
     <>
       <Header />
-      <main>{children}</main>
-      <footer>FOOTER</footer>
+
+      {children}
+
+      <Footer />
     </>
   );
 }
