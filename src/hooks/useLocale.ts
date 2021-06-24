036a5d995861b44254653c47c36810222ad6c7e0
David Wheatley <hi@davwheat.dev>
2021-06-24 01:39:50 +0100

    Fix broken useLocale hook

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,9 +1,9 @@
 import { useRecoilValue } from 'recoil';
-import { AvailableLocales, LocaleClass } from '@locales';
+import { LocaleClass, LocalesByLangCode } from '@locales';
 import { LocaleAtom } from '@atoms';
 
 export default function useLocale(): LocaleClass {
   const localeCode = useRecoilValue(LocaleAtom);
 
-  return AvailableLocales[localeCode].getLocale() as LocaleClass;
+  return LocalesByLangCode[localeCode];
 }
