f67c9b3203200e778859ecf9944a0d4992ec3f59
David Wheatley <hi@davwheat.dev>
2021-06-24 02:00:28 +0100

    Use new footer exports

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -4,13 +4,20 @@ import { Link } from '@components/Link';
 
 import PackageJson from '../../../package.json';
 
-export default function Footer() {
+interface IFooterProps {
+  className?: string;
+  innerClassName?: string;
+}
+
+export function Footer({ className, innerClassName }: IFooterProps) {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Stichting Flarum (Flarum Foundation). Licensed under the MIT license.</p>
-      <p>
-        Docs site version {PackageJson.version}. <Link url="https://github.com/flarum/docs">View this site on GitHub</Link>
-      </p>
+    <footer className={className}>
+      <div className={innerClassName}>
+        <p>&copy; {new Date().getFullYear()} Stichting Flarum (Flarum Foundation). Licensed under the MIT license.</p>
+        <p>
+          Docs site version {PackageJson.version}. <Link url="https://github.com/flarum/docs">View this site on GitHub</Link>
+        </p>
+      </div>
     </footer>
   );
 }
